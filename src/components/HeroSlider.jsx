d20c05ab9d2c803f7d7da59447c5bcd2983ebbbb
Smriti Jain <iti.smriti23@gmail.com>
2025-09-24 11:37:01 +0530

    Initial Commit

--- src/components/HeroSlider.jsx
+++ src/components/HeroSlider.jsx
@@ -3,23 +3,24 @@ import React from 'react';
 export default function HeroSlider() {
   return (
     <div className="bg-sky-400 p-10 text-white flex justify-between items-center relative overflow-hidden h-72">
-      
+
 
       {/* Content Section */}
       <div className="z-10">
         <h3 className="text-2xl font-bold mt-10 max-w-xs">Adidas Men Running Sneakers</h3>
         <p className=" text-xs max-w-md">Performance and design. Taken right to the edge.</p>
-        <button className="mt-6 px-4 py-2 bg-white text-sky-400 rounded-full font-semibold hover:bg-gray-100 transition-colors">
+        <button className="mt-6 py-2 text-xs text-white-400 font-semibold">
           SHOP NOW
+          <div className=" w-3/5 h-0.5 bg-white"></div>
         </button>
       </div>
 
       {/* Image Section */}
       <div className="z-10">
         <img
-          src="https://i.postimg.cc/Qx6qgnRH/Shoe.png"
+          src="https://i.postimg.cc/hGRnd66r/Shoe-png.png"
           alt="Running Shoe"
-          className="w-96 -rotate-360"
+          className="w-96 pt-20 pl-3"
         />
       </div>
 
